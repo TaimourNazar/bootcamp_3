fc7898a8d087823e92bbbea030a6d737378485fa
Taimour <taimour.nazar1@gmail.com>
2020-07-07 12:26:36 +0500

    assignment_class3_updated_2

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -5,7 +5,7 @@ function MediaCard({title,body,imageUrl}) {
 return <div>
             <h2>{title}</h2>
             <p className="myname">{body}</p>
-            <img src={imageUrl}/>
+            <img alt="Image Url" src={imageUrl}/>
        </div>
 }
 
